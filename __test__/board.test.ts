edb1b650f7d7a40637cd85f3984dd06b6c48041b
Stary_Algida <piotrwc49@gmail.com>
2023-09-19 18:18:37 +0200

    repair field test

--- __test__/board.test.ts
+++ __test__/board.test.ts
@@ -3,7 +3,7 @@ import { createField } from "../src/objects/fieldObject";
 test("create Field", () => {
   const div = document.createElement("div");
   expect(createField([1, 2], div)).toStrictEqual({
-    id: 1,
+    id: [1, 2],
     field: expect.any(Element),
     shooted: false,
     ship: false,
